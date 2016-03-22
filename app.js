9796e62d3faca28bd019571d5610ed2d31130c69
swimbo <bo.bergstrom@gmail.com>
2016-03-22 15:50:03 -0600

    localized all source links due to slow network issues. Added draft ui-router framework

--- app.js
+++ app.js
@@ -4,27 +4,27 @@
   // Declare the module
   //======================================================//
 angular.module('relationshipMinder', ['ngAnimate', 'ui.router'])
-  // =======================================================//
-  // Declare the factory
-  // =======================================================//
-angular.module('relationshipMinder')
-      .factory('contactFactory', contactFactory)
+//   // =======================================================//
+//   // Declare the factory
+//   // =======================================================//
+// angular.module('relationshipMinder')
+//       .factory('contactFactory', contactFactory)
   // =======================================================//
   // Declare our controller for the webapp flow
   // =======================================================//
 angular.module('relationshipMinder')
-        .controller('rmController', rmController)
+        .controller('RelationshipMinderController', RelationshipMinderController)
 
 var contactList = []
 console.log(contactList);
 
-// this factory stores data outside of the controller(s)
-function contactFactory(){
-    var contactList = [ ]
-    return(contactList)
-  }
+// // this factory stores data outside of the controller(s)
+// function contactFactory(){
+//     var contactList = [ ]
+//     return(contactList)
+//   }
 
-function rmController (){
+function RelationshipMinderController (){
   var rmCont = this
 
   //object constructor to create new contact objects based on API connections and/or front-end clicks/actions
@@ -38,7 +38,6 @@ function rmController (){
 
   // factory to turn our contacts into objects with durations
   function contactBucket(){
-
     console.log(contactList);
   }
 
@@ -48,43 +47,50 @@ function rmController (){
 
 
   //======================================================//
-  // Configure our routes
+  //Configure our routes
   //======================================================//
-    // .config(function($stateProvider, $urlRouterProvider) {
-    //
-    //   $stateProvider
-    //
-    //   // Route to show the first page
-    //
-    //   .state('getting-started', {
-    //         url: '/getting-started',
-    //         templateUrl: 'getting-started.html',
-    //         controller: 'rmController as rmControl'
-    //     })
-    //     // nested states
-    //     // each of these sections will have their own view
-    //     // url will be nested (/getting-started/connect)
-    //    .state('getting-started.connect', {
-    //        url: '/connect',
-    //        templateUrl: 'connect.html'
-    //    })
-    //
-    //    // url will be /getting-started/buckets
-    //    .state('getting-started.buckets', {
-    //        url: '/buckets',
-    //        templateUrl: 'buckets.html'
-    //    })
-    //
-    //    // url will be /getting-started/notifications
-    //    .state('getting-started.notifications', {
-    //        url: '/notifications',
-    //        templateUrl: 'notifications.html'
-    //    });
-    //
-    //        // catch all route
-    //        // send users to the form page
-    //        $urlRouterProvider.otherwise('/getting-started');
-    //    })
+  angular.module('relationshipMinder')
+    .config(function($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+
+      // Route to show the first page
+
+      .state('getting-started', {
+            url: '/getting-started',
+            templateUrl: 'getting-started.html',
+            controller: 'rmController as rmControl'
+        })
+        // nested states
+        // each of these sections will have their own view
+        // url will be nested (/getting-started/connect)
+       .state('getting-started.connect', {
+           url: '/connect',
+           templateUrl: 'connect.html'
+       })
+
+       // url will be /getting-started/buckets
+       .state('getting-started.buckets', {
+           url: '/buckets',
+           templateUrl: 'buckets.html'
+       })
+
+       // url will be /getting-started/notifications
+       .state('getting-started.notifications', {
+           url: '/notifications',
+           templateUrl: 'notifications.html'
+       });
+
+       // url will be /activity-feed.html
+       .state('activity-feed', {
+           url: '/activity-feed',
+           templateUrl: 'activity-feed.html'
+       });
+
+           // catch all route
+           // send users to the activity-feed
+           $urlRouterProvider.otherwise('/activity-feed');
+       })
 
 
 })();
