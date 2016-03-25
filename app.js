f4b2a1ab173c95c38e63043414c0ae1d9d00d418
swimbo <bo.bergstrom@gmail.com>
2016-03-24 23:50:51 -0600

    top styling complete.

--- app.js
+++ app.js
@@ -41,10 +41,11 @@
     console.log(vmRMCtrl);
 
     //object constructor to create new contact objects based on API connections and/or front-end clicks/actions
-    function contactItem(firstName, lastName, email, lastContact, bucket, overdue, daysSince) {
+    function contactItem(firstName, lastName, email, phone, lastContact, bucket, overdue, daysSince) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.phone = phone;
       this.lastContact = lastContact;
       this.bucket = bucket;
       this.overdue = overdue;
@@ -56,11 +57,11 @@
     // Contacts Page starts here
     // =======================================================//
     //fake contacts for testing. To be replaced by Google API data
-    var contact1 = new contactItem('firstname1', 'lastname1', 'test1@gmail.com', 1332302400000)
-    var contact2 = new contactItem('firstname2', 'lastname2', 'test2@gmail.com', 1294862756114)
-    var contact3 = new contactItem('firstname3', 'lastname3', 'test3@gmail.com',  878404500000)
-    var contact4 = new contactItem('firstname4', 'lastname4', 'test4@gmail.com', 1332302400000)
-    var contact5 = new contactItem('firstname5', 'lastname5', 'test5@gmail.com', 1458799200000)
+    var contact1 = new contactItem('Poor Old', 'Grandma', 'test1@gmail.com', '555-555-5555', 1332302400000)
+    var contact2 = new contactItem('James', 'lastname2', 'test2@gmail.com', '555-555-5555', 1294862756114)
+    var contact3 = new contactItem('firstname3', 'lastname3', 'test3@gmail.com', '555-555-5555', 878404500000)
+    var contact4 = new contactItem('firstname4', 'lastname4', 'test4@gmail.com', '555-555-5555', 1332302400000)
+    var contact5 = new contactItem('firstname5', 'lastname5', 'test5@gmail.com', '555-555-5555', 1458799200000)
 
     //fake array for testing.
     vmRMCtrl.googList = [contact1, contact2, contact3, contact4, contact5]
@@ -147,6 +148,73 @@
   //     console.log(contactList.name);
   //   }
   //
+  //======================================================//
+  //Google Login Code
+  //======================================================//
+  function gPOnLoad(){
+       // G+ api loaded
+       document.getElementById('gp_login').style.display = 'block';
+  }
+  vmRMCtrl.googleAuth = function() {
+      gapi.auth.signIn({
+          callback: gPSignInCallback,
+          clientid: googleKey,
+          cookiepolicy: "single_host_origin",
+          requestvisibleactions: "http://schema.org/AddAction",
+          scope: "https://www.googleapis.com/auth/plus.login email"
+      })
+  }
+
+  function gPSignInCallback(e) {
+      if (e["status"]["signed_in"]) {
+          gapi.client.load("plus", "v1", function() {
+              if (e["access_token"]) {
+                  getProfile()
+              } else if (e["error"]) {
+                  console.log("There was an error: " + e["error"])
+              }
+          })
+      } else {
+          console.log("Sign-in state: " + e["error"])
+      }
+  }
+
+  function getProfile() {
+      var e = gapi.client.plus.people.get({
+          userId: "me"
+      });
+      e.execute(function(e) {
+          if (e.error) {
+              console.log(e.message);
+              return
+          } else if (e.id) {
+              // save profile data
+          }
+      })
+  }(function() {
+      var e = document.createElement("script");
+      e.type = "text/javascript";
+      e.async = true;
+      e.src = "https://apis.google.com/js/client:platform.js?onload=gPOnLoad";
+      var t = document.getElementsByTagName("script")[0];
+      t.parentNode.insertBefore(e, t)
+  })()
+
+
+
+  vmRMCtrl.googlePeopleAPIcontroller($http){
+    var GoogAPI = this
+
+    $http.get('https://accounts.google.com/o/oauth2/v2/auth?client_id=199009851105-j9aosg5ru9knh1rje5acp0qav5s5ant5.apps.googleusercontent.com&response_type=token&redirect_uri=https://thekidgarage.com/membership/&scope=email%20profile', {cache: true})
+      .then(function(response){
+        console.log(response.data)
+      })
+  }
+
+
+
+
+
   }
 
 
@@ -198,6 +266,8 @@ angular.module('relationshipMinder')
 
       $urlRouterProvider.otherwise('/landing');
     })
+
+
 
 
 })();
