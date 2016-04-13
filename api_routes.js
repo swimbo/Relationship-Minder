527888acad75f8bc1a0316bcc9b1372ea00eaab6
swimbo <bo.bergstrom@gmail.com>
2016-04-13 11:34:14 -0600

    refactored to make it modular. app runs, but db not fully connected.

---
+++ api_routes.js
@@ -0,0 +1,16 @@
+var
+  apiRouter = require('express').Router(),// setting the apiRouter variable to express.Router()
+  ctrl      = require('./controllers/relationship-minder-controller') // requiring the relationship-minder-controller and setting it to the variable ctrl
+
+  apiRouter.route('/relationship-minder')
+    .get(ctrl.rmController.getAll)
+    .post(ctrl.rmController.create)
+
+   apiRouter.route('/relationship-minder/:id')
+  //http://localhost:3000/api/v1/relationship-minder/:valueHere
+    .get(ctrl.rmController.getSingle)
+    .put(ctrl.rmController.update)
+    .delete(ctrl.rmController.destroy)
+
+
+module.exports  = apiRouter
---
+++ models/relationship-minder-model.js
@@ -0,0 +1,21 @@
+var mongoose       = require('mongoose'),      // <-- assigning mongoose to the variable, mongoose
+  Schema           = mongoose.Schema,          // creating a Schema variable that points to mongoose's Schema method
+  rmSchema = new Schema({              // Creating a reuseable rm Schema, using the new keyword, along with the Schema variable that is linked to mongoose's Schema method
+      firstName    : {type: String, required: true},  // <-- setting a data type for the first name key, and setting it to validate that a string is put in
+      lastName     :  String,  // <-- setting a data type for the last name key
+      emailAddress :  String,  // <-- setting the data type for the email address key to string
+      phoneNumber  :  String,  // <-- setting the phone number type for the email address key to string
+      lastContact  : {type: Number, min: 0}, // <-- setting the data type for the last contact key to number with a minimum number of 0
+      bucket       :  Number, //<-- setting the data type for the bucket key to number.
+      overdue      :  Boolean,                          // <-- setting the data type for the address key to string
+      daysSince    : {type: Number, min: 0},  // <-- setting a data type for the days since key, and setting it to validate that a number greater 0
+
+      createdAt    : {type: Date, Default: Date.now}, // setting the createdAt key to automatically assign the specific day date to createdAt
+      nextContact  :  Date
+
+  })
+
+
+  module.exports = { // turning the variable rmSchema into a mongoose model, assigning it to the rmModel key inside the module.exports object
+    rmModel : mongoose.model('rmModel', rmSchema)
+  }
